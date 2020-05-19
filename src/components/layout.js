123c2914a1910635bc387afce43a9c48c45ef8b8
MDzkM <mdzkm17@gmail.com>
2020-05-19 21:44:19 +0700

    Tweak footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,12 @@ import Header from "./header"
 import "./layout.css"
 import 'bootstrap/dist/css/bootstrap.css'
 
+import { Icon } from "@iconify/react"
+import socialGithubCircular from "@iconify/icons-typcn/social-github-circular"
+import gitlabCircle from "@iconify/icons-jam/gitlab-circle"
+import youtubeCircle from "@iconify/icons-jam/youtube-circle"
+import linkedinCircle from "@iconify/icons-jam/linkedin-circle"
+
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './theme';
 import { GlobalStyles } from './global';
@@ -52,10 +58,14 @@ class Layout extends Component {
             }}
           >
             <main>{children}</main>
-            <footer className="mt-auto" style={{padding: `5% 0`}}>
-              © {new Date().getFullYear()} MDzkM. Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>.
+            <footer className="mt-auto" style={{width: `100%`, padding: `5% 0`, display: `flex`, flexDirection: `row`, alignItems: `center`}}>
+              © {new Date().getFullYear()} mdzkm.
+              <div className="footer-icons ml-auto">
+                  <a href="https://www.github.com/MDzkM" target="_blank" rel="noopener noreferrer" title="Github"><Icon icon={socialGithubCircular} style={{fontSize: '53.33px'}} /></a>
+                  <a href="https://www.gitlab.com/MDzkM" target="_blank" rel="noopener noreferrer" title="Gitlab"><Icon icon={gitlabCircle} style={{fontSize: '48px'}} /></a>
+                  <a href="https://www.youtube.com/channel/UCIeCJ3CGn5ciVIEDNzkuABg" target="_blank" rel="noopener noreferrer" title="Youtube"><Icon icon={youtubeCircle} style={{fontSize: '48px'}} /></a>
+                  <a href="https://www.linkedin.com/in/MDzkM" target="_blank" rel="noopener noreferrer" title="LinkedIn"><Icon icon={linkedinCircle} style={{fontSize: '48px'}} /></a>
+              </div>
             </footer>
           </div>
         </>
